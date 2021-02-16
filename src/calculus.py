3dfd6d98d1aa95f4b719d045efbf56e24b649e07
Daniel Svitan <dankosaurus9@gmail.com>
2021-02-16 19:52:24 +0100

    finally found right algorithm

---
+++ src/calculus.py
@@ -0,0 +1,25 @@
+import math
+
+
+def test() -> any:
+    #      c
+    # A _______ B
+    #  \       |
+    #   \      |
+    #    \     |
+    #     \    |
+    #    b \   | a
+    #       \  |
+    #        \ |
+    #         \|
+    #          C
+    c = 3
+    a = 4
+    b = 5
+
+    print(a / b)
+    print(2 * (b / a))
+
+
+if __name__ == '__main__':
+    test()
